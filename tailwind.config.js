7bbac646136148d71e881875f228e4c708aaebc8
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-08-09 11:57:02 +0700

    setup project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "15px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
     extend: {
+      colors: {
+        primary: "#161727",
+        secondary: "#42434E",
+        accent: "#F16324",
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        site: "url('/bg.jpg')",
+      },
+      animation: {
+        "spin-slow": "spin 6s linear infinite",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
-  plugins: [],
+  container: {
+    padding: {
+      DEFAULT: "15px",
+    },
+  },
+  plugins: [require("tailwind-scrollbar")],
 };
