fa26c03ddf347856dd2b95ddc475d2746e96bdc7
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-08-11 08:07:28 +0700

    add index

--- components/Circle.js
+++ components/Circle.js
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { RiRadioButtonLine } from "react-icons/ri";
 const Circle = () => {
   return (
     <div>
-      
+      <Link
+        href="/"
+        className="relative w-[185px] h-[185px] flex justify-center items-center"
+      >
+        <Image
+          src={"/hero-banner.png"}
+          alt="circle"
+          width={141}
+          height={148}
+          className="animate-spin-slow max-w-[141px] max-h-[141px] w-full h-full"
+        />
+        <RiRadioButtonLine className="absolute text-4xl text-accent animate-spin-slow" />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Circle
+export default Circle;
--- components/Transition.js
+++ components/Transition.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
 
+import { motion } from "framer-motion";
+
+const transitionVariants = {
+  initial: {
+    x: "100%",
+    width: "100%",
+  },
+  animate: {
+    x: "0%",
+    width: "0%",
+  },
+  exit: {
+    x: ["0%", "100%"],
+    width: ["0%", "100%"],
+  },
+};
 const Transition = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#ffffff]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+      ></motion.div>
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#ff9800]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
+      ></motion.div>
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#370909]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
+      ></motion.div>
+    </>
+  );
+};
 
-export default Transition
+export default Transition;
